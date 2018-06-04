e89133ca7572e2a6abf43fc61b3afc39a6c11e7c
Jorge Raul Solis Rangel <JorgeRaul@MacBook-Pro-de-Jorge.local>
2018-06-03 20:37:16 -0500

    confirm

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,14 @@
+import {Component,OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-shopping-edit',
+    templateUrl: './shopping-edit.component.html'
+})
+
+export class ShoppingListEditComponent implements OnInit{
+
+    constructor() {}
+    ngOnInit(){
+
+    }
+}
