98a78db000469e021341058baa19a5699264b594
Jorge Solis <solisjorge1302@gmail.com>
2018-06-04 17:39:12 -0500

    Section 3 Lecture 48
    Modify Recipe model and recipe-list component to display recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is a simple test','https://pixabay.com/p-1459693/?no_redirect'),
+    new Recipe('Another Recipe','This is another simple test','https://www.flickr.com/photos/missrogue/8546806986')
+
+  ];
   constructor() { }
 
   ngOnInit() {
