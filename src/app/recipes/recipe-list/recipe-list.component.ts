d25587d6ff212c8cd18337ccaea6be1785b61e18
Jorge Solis <solisjorge1302@gmail.com>
2018-06-13 17:51:18 -0500

    Section 6 Lecture 79
    Adding functionallity to the recipe-item-component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
     new Recipe('A Test Recipe','This is a simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Recipe','This is another simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Another Recipe','This is another simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('Third Recipe','This is third simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
 
   ];
   constructor() { }
