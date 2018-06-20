a8725875d8317c50839a91ddf5e0b7a735b4b706
Jorge Solis <solisjorge1302@gmail.com>
2018-06-20 17:43:22 -0500

    Section 6  Final changes
    Changes prev to lecture 7

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,25 +1,34 @@
-import {Component,OnInit,ViewChild, EventEmitter,Output, ElementRef} from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ElementRef,
+  ViewChild,
+  EventEmitter,
+  Output
+} from '@angular/core';
+
 import { Ingredient } from '../../shared/ingredient.model';
 
-
 @Component({
-    selector: 'app-shopping-edit',
-    templateUrl: './shopping-edit.component.html'
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html'
+  
 })
+export class ShoppingListEditComponent implements OnInit {
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-export class ShoppingListEditComponent implements OnInit{
-    @ViewChild('nameInput') nameInputRef: ElementRef ;
-    @ViewChild('amountInput') amountInputRef: ElementRef ;
-    @Output() ingredientAdded = new EventEmitter<Ingredient>();
-    constructor() {}
-    ngOnInit(){
+  constructor() { }
 
-    }
-    onAddItem(nameInput: HTMLInputElement){
-        const ingName = this.nameInputRef.nativeElement.value;
-        const ingAmount = this.amountInputRef.nativeElement.value;
-        const newIngredient = new Ingredient(ingName,ingAmount);
-        this.ingredientAdded.emit(newIngredient);
+  ngOnInit() {
+  }
 
-    }
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
+
 }
