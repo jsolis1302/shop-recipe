ac1f9dcdc45d6fef6ca394c1b8ecb8c92f1f4020
Jorge Solis <solisjorge1302@gmail.com>
2018-06-20 17:56:46 -0500

    Section 6 Lecture 81
    Import Form module in order to prevent the page to reload after clicking
    add button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,7 +24,8 @@ import { RecipesComponent } from './recipes/recipes.component';
 
   ],
   imports: [ 
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
