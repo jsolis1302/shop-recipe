3326d2001440232bfe345615a37f2955d8fc6781
Jorge Solis <solisjorge1302@gmail.com>
2018-07-26 09:45:54 -0500

    Section 8 Lecture 95
    Custom directive for drop down

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -8,7 +8,8 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
-
+  //toggled = false;
+  
   constructor() { }
 
   ngOnInit() {
