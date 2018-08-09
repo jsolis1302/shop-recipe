5c8a37ea2237dbb4e0da19923d8a61d327430091
Jorge Solis <solisjorge1302@gmail.com>
2018-08-09 13:28:49 -0500

    Section 10 Lecture 108
    Services for Crossing Components

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,13 +9,14 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() item: Recipe;
-  @Output() recipeSelected = new EventEmitter<void>();
-  constructor() { }
+  //@Output() recipeSelected = new EventEmitter<void>();
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
   onSelected(recipe: Recipe){
-    this.recipeSelected.emit();
+    //this.recipeSelected.emit();
+    this.recipeService.recipeSelected.emit(this.item);
 
   }
 
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from "./recipe.model";
+import { EventEmitter } from "@angular/core";
 
 export class RecipeService {
 
+   recipeSelected = new EventEmitter<Recipe>();
    private recipes: Recipe[] = [
         new Recipe('A Test Recipe','This is a simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
         new Recipe('Another Recipe','This is another simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
