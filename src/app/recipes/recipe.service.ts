d02296038241ce03ad422e0b384705674315284f
Jorge Solis <solisjorge1302@gmail.com>
2018-08-15 12:17:03 -0500

    Section 10 Lecture 111
    Adding ingredientes into the recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,25 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
 
    recipeSelected = new EventEmitter<Recipe>();
    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe','This is a simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Recipe','This is another simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Third Recipe','This is third simple test','https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('Tasty Schnitzel',
+                   'A super-tasty Schnitzel - just awesome!',
+                   'https://upload.wikimedia.org/wikipedia/commons/9/90/2015_0723_Wiener_Schnitzel_vom_Schwein_Gaislachalm_S%C3%B6lden.jpg',
+                    [
+                        new Ingredient('Meat',1),
+                        new Ingredient('French Fries',20)
+                    ]),
+        new Recipe('Big Fat Burger',
+                    'What else you need to say',
+                    'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+                     [
+                        new Ingredient('Buns',2),
+                        new Ingredient('Meat',1)
+                     ]),
     
       ];
 
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -7,6 +7,10 @@ export class ShoppingListService {
     private ingredients: Ingredient[] = [
         new Ingredient('Apples',5),
         new Ingredient('Tomatoes',10),
+        new Ingredient('Oranges',8),
+        new Ingredient('Bread',2),
+        new Ingredient('Carrots',12),
+        new Ingredient('Lemons',10)
     ];
     
     getIngredients() {
