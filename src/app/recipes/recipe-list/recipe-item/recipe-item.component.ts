54ea076f1a0c8e4b1e9b4d1e079a4b6f65cdbb80
Jorge Solis <solisjorge1302@gmail.com>
2018-08-29 17:05:21 -0500

    Section 12 LEcture 152
    adding id into the route

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,14 +10,14 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() item: Recipe;
   //@Output() recipeSelected = new EventEmitter<void>();
-  constructor(private recipeService: RecipeService) { }
+  //constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
-  onSelected(recipe: Recipe){
-    //this.recipeSelected.emit();
-    this.recipeService.recipeSelected.emit(this.item);
+  // onSelected(recipe: Recipe){
+  //   //this.recipeSelected.emit();
+  //   this.recipeService.recipeSelected.emit(this.item);
 
-  }
+  // }
 
 }
