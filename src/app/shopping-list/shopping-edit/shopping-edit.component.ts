10a972e9c93eff3df82233f8f185cb7dc0a3f996
Jorge Solis <solisjorge1302@gmail.com>
2019-04-15 13:17:07 -0500

    Lecture 210, Section 16
    Adding click listeners to edit mode

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -3,27 +3,31 @@ import {
   OnInit,
   ElementRef,
   ViewChild,
+  OnDestroy,
 
 } from '@angular/core';
 
 import { Ingredient } from '../../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html'
   
 })
-export class ShoppingListEditComponent implements OnInit {
+export class ShoppingListEditComponent implements OnInit, OnDestroy {
   //@ViewChild('nameInput') nameInputRef: ElementRef;
   //@ViewChild('amountInput') amountInputRef: ElementRef;
+  subscription: Subscription;
   
   
 
   constructor(private shoppingListService: ShoppingListService) { }
 
   ngOnInit() {
+   this.subscription= this.shoppingListService.startedEditing.subscribe();
   }
 
   onAddItem(form: NgForm) {
@@ -33,6 +37,10 @@ export class ShoppingListEditComponent implements OnInit {
     this.shoppingListService.addIngredient(newIngredient);
     console.log(form.value);
     
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
