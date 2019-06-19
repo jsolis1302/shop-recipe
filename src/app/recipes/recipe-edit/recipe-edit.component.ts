78d8a3aa994675ec3b2a28707f91e04f6cc3913a
Jorge Solis <solisjorge1302@gmail.com>
2019-06-19 17:20:03 -0500

    Section 16, Lecture 231
    cancel, and delete button

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
 import { Recipe } from '../recipe.model';
@@ -14,7 +14,7 @@ export class RecipeEditComponent implements OnInit {
   editMode = false;
   recipeForm: FormGroup;
 
-  constructor(private route: ActivatedRoute, private recipeService: RecipeService) { }
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService, private router:Router) { }
 
   ngOnInit() {
     this.route.params.subscribe(
@@ -82,6 +82,7 @@ export class RecipeEditComponent implements OnInit {
       //this.recipeService.addRecipe(newRecipe);
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
 
   onAddIngredient(){
@@ -93,6 +94,10 @@ export class RecipeEditComponent implements OnInit {
       }
     )
    );
+  }
+
+  onCancel(){
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 
 }
