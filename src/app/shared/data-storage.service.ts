50b46b8c592d3b746e90feb141883a7ad42fc139
Jorge Solis <jorge.solis@4thsource.com>
2019-07-22 16:59:36 -0500

    Lecture 279
    Storing Recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({providedIn:'root'})
-export class DataStorageService{
-    constructor(private http: HttpClient){}
+export class DataStorageService{ 
+    constructor(private http: HttpClient, private recipeService: RecipeService){}
+    storeRecipes() {
+        const recipies = this.recipeService.getRecipes();
+        this.http.put('https://ng-course-recipe-book-f735a.firebaseio.com/recipes.json',recipies)
+        .subscribe(response => {
+            console.log(response);
+        });
+    }
 
 }
