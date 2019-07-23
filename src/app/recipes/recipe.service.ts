6f0c9944027ed657dd939603d1f69aa5ae62a97f
Jorge Solis <jorge.solis@4thsource.com>
2019-07-23 12:21:10 -0500

    Lecture 281
    Transforming data to set the ingredients to empty array if the recipe dos not have any ingredinet

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,23 +8,24 @@ import { Subject } from "rxjs";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
    //recipeSelected = new EventEmitter<Recipe>();
-   private recipes: Recipe[] = [
-        new Recipe('Tasty Schnitzel',
-                   'A super-tasty Schnitzel - just awesome!',
-                   'https://upload.wikimedia.org/wikipedia/commons/9/90/2015_0723_Wiener_Schnitzel_vom_Schwein_Gaislachalm_S%C3%B6lden.jpg',
-                    [
-                        new Ingredient('Meat',1),
-                        new Ingredient('French Fries',20)
-                    ]),
-        new Recipe('Big Fat Burger',
-                    'What else you need to say',
-                    'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
-                     [
-                        new Ingredient('Buns',2),
-                        new Ingredient('Meat',1)
-                     ]),
+//    private recipes: Recipe[] = [
+//         new Recipe('Tasty Schnitzel',
+//                    'A super-tasty Schnitzel - just awesome!',
+//                    'https://upload.wikimedia.org/wikipedia/commons/9/90/2015_0723_Wiener_Schnitzel_vom_Schwein_Gaislachalm_S%C3%B6lden.jpg',
+//                     [
+//                         new Ingredient('Meat',1),
+//                         new Ingredient('French Fries',20)
+//                     ]),
+//         new Recipe('Big Fat Burger',
+//                     'What else you need to say',
+//                     'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+//                      [
+//                         new Ingredient('Buns',2),
+//                         new Ingredient('Meat',1)
+//                      ]),
     
-      ];
+//       ];
+      private recipes: Recipe[] = [];
     
     constructor(private slService: ShoppingListService){
 
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
 
+import { map } from "rxjs/operators";
+
 @Injectable({providedIn:'root'})
 export class DataStorageService{ 
     constructor(private http: HttpClient, private recipeService: RecipeService){}
@@ -16,7 +18,19 @@ export class DataStorageService{
 
     fetchRecipes(){
         this.http
-        .get<Recipe[]>('https://ng-course-recipe-book-f735a.firebaseio.com/recipes.json')
+        .get<Recipe[]>('https://ng-course-recipe-book-f735a.firebaseio.com/recipes.json'
+        ).pipe(
+            map(recipes =>{
+                return recipes.map(recipe => {
+                    return {
+                        ...recipe,
+                        ingredients: recipe.ingredients ? recipe.ingredients : []
+                    };
+                }); 
+
+                
+            })
+        )
         .subscribe(recipes => {
             //console.log(recipies);
             this.recipeService.setRecipes(recipes);
