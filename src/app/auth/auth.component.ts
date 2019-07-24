0d2e9ca7533de234cf5bb34ad184ed6afa4b34be
Jorge Solis <jorge.solis@4thsource.com>
2019-07-24 13:27:00 -0500

    lecture 292
    Sending the signup request

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { from } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-auth',
@@ -9,7 +10,7 @@ import { from } from 'rxjs';
 })
 export class AuthComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   isLoginMode = true;
   ngOnInit() {
@@ -18,8 +19,27 @@ export class AuthComponent implements OnInit {
   onSwitchMode(){
     this.isLoginMode = !this.isLoginMode;
   }
+
   onSubmit(form: NgForm){
-    console.log(form.value);
+    if(!form.valid) return ;
+    const email = form.value.email;
+    const password = form.value.password;
+    if (this.isLoginMode){
+      //...
+    } else {
+      this.authService.signup(email,password)
+      .subscribe(resData=> {
+          console.log(resData);
+        },
+        error => {
+          console.log(error);
+        }
+  
+      );
+    }
+
+    
+    
     form.reset();
 
   }
