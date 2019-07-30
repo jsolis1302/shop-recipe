9515377aaf83c8f784b76c39d936d73175eff0d1
Jorge Solis <jorge.solis@4thsource.com>
2019-07-30 12:47:24 -0500

    Lecture 298
    Additional changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   // @Output() featureSelected = new EventEmitter<string>();
 
   // onSelect(feature: string) {
   //   this.featureSelected.emit(feature);
   //}
-  constructor(private dataStorageService: DataStorageService, authService: AuthService) {}
+  isAuthenticated = false;
+  private userSub: Subscription;
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) {}
 
   ngOnInit(){
-    
+    this.userSub = this.authService.user.subscribe(
+      user=>{
+        this.isAuthenticated = !!user;
+        console.log(!user);
+        console.log(!!user);
+      });
+  }
+
+  ngOnDestroy(){
+    this.userSub.unsubscribe();
   }
   onSaveData(){
     this.dataStorageService.storeRecipes();
