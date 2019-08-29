47515e7ad85190f53d7694ffab75e4003deeb6d4
Jorge Solis <jorge.solis@4thsource.com>
2019-08-29 13:43:33 -0500

    Lecture 349
    Setting up the NgRx Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { StoreModule } from '@ngrx/store'
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -17,6 +19,7 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    StoreModule.forRoot({shoppingList: shoppingListReducer}),
     SharedModule,
     CoreModule,
   ],
