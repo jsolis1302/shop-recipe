827fdec9c44396e055ff184ec01b22cd7860d849
Jorge Solis <jorge.solis@4thsource.com>
2019-09-05 09:58:31 -0500

    Lecture 355
    Expanding the state

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -2,14 +2,27 @@ import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions';
 import { Action } from '@ngrx/store';
 
-const initialState = {
+export interface AppState{
+    shoppingList: State;
+}
+
+export interface State {
+    ingredients: Ingredient[];
+    editedIngredient: Ingredient;
+    editedIngredientIndex: number;
+}
+
+const initialState: State = {
     ingredients: [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
-      ]
+      ],
+      editedIngredient: null,
+      editedIngredientIndex: -1,
+
 };
 
-export function shoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
+export function shoppingListReducer(state: State = initialState, action: ShoppingListActions.ShoppingListActions) {
     switch(action.type){
         case ShoppingListActions.ADD_INGREDIENT: 
             return {
