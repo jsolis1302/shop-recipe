0b8277caa1b193d4ff1cfd8cbe507ec5b5ab7d4b
Jorge Solis <jorge.solis@4thsource.com>
2019-09-06 10:12:53 -0500

    Lecture 356
    managing more state via NgRx

--- src/app/shopping-list/store/shopping-list.actions.ts
+++ src/app/shopping-list/store/shopping-list.actions.ts
@@ -6,6 +6,8 @@ export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
 export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
 export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export const START_EDIT = 'START_EDIT';
+export const STOP_EDIT = 'STOP_EDIT';
 
 export class AddIngredient implements Action{
     readonly type = ADD_INGREDIENT;
@@ -33,6 +35,23 @@ export class DeleteIngredient implements Action{
 
 }
 
+export class StartEdit implements Action{
+    readonly type = START_EDIT;
+
+    constructor(public paylod: number){
+
+    }
+}
+export class StopEdit implements Action {
+    readonly type = STOP_EDIT;
+}
 
 
-export type ShoppingListActions = AddIngredient | AddIngredients | UpdateIngredient | DeleteIngredient;
+
+export type ShoppingListActions = 
+AddIngredient | 
+AddIngredients | 
+UpdateIngredient | 
+DeleteIngredient |
+StartEdit |
+StopEdit;
