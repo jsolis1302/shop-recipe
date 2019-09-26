e4bb0868b49170da7901c2438bc495fc2a9874a0
Jorge Solis <jorge.solis@4thsource.com>
2019-09-26 15:41:49 -0500

    Lecture 360
    setting up auth reducer and actions

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -11,5 +11,25 @@ const initialState = {
 };
 
 export function authReducer(state = initialState, action: AuthActions.AuthActions){
-    return state;
+    switch(action.type){
+        case AuthActions.LOGIN:
+            const user = new User(
+                action.payload.email, 
+                action.payload.userId, 
+                action.payload.token, 
+                action.payload.expirationDate
+            );
+
+            return {
+                ...state,
+                user: user
+            };
+        case AuthActions.LOGOUT:
+            return {
+                ...state,
+                user:null
+            };
+        default: 
+            return state;
+    }
 }
