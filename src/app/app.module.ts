90aa4f9860691646894e12be0c7b942ffbd33d44
Jorge Solis <solisjorge1302@gmail.com>
2019-10-28 23:09:03 -0600

    Lecture 378
    The Router Store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { StoreModule } from '@ngrx/store'
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
 import { environment } from '../environments/environment';
@@ -27,6 +27,7 @@ import { environment } from '../environments/environment';
     StoreModule.forRoot( fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument({logOnly: environment.production}),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule,
   ],
