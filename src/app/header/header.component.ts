099ddc7d98ab4d7dbe79017802eb434cc1be45e7
Jorge Solis <jorge.solis@4thsource.com>
2019-10-29 13:21:30 -0600

    Lecture 381
    Fetching Recipes and using the resolver

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,6 +8,7 @@ import { map } from 'rxjs/operators';
 
 import * as fromApp from '../store/app.reducer';
 import * as AuthActions from '../auth/store/auth.actions';
+import * as RecipesActions from '../recipes/store/recipe.actions';
 
 @Component({
   selector: 'app-header',
@@ -38,7 +39,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   }
 
   onFetchData() {
-    this.dataStorageService.fetchRecipes().subscribe();
+    //this.dataStorageService.fetchRecipes().subscribe();
+    this.store.dispatch(new RecipesActions.FetchRecipes());
   }
 
   onLogout() {
--- src/app/recipes/recipes-resolver.service.ts
+++ src/app/recipes/recipes-resolver.service.ts
@@ -8,19 +8,33 @@ import {
 import { Recipe } from './recipe.model';
 import { DataStorageService } from '../shared/data-storage.service';
 import { RecipeService } from './recipe.service';
+import { Store } from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducer';
+import * as RecipesActions from '../recipes/store/recipe.actions';
+import { Actions, ofType } from '@ngrx/effects';
+import { take } from 'rxjs/operators';
+
 
 @Injectable({ providedIn: 'root' })
 export class RecipesResolverService implements Resolve<Recipe[]> {
   constructor(
-    private dataStorageService: DataStorageService,
-    private recipesService: RecipeService
+    
+    private recipesService: RecipeService,
+    private store: Store<fromApp.AppState>,
+    private actions$ : Actions
   ) {}
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const recipes = this.recipesService.getRecipes();
 
     if (recipes.length === 0) {
-      return this.dataStorageService.fetchRecipes();
+      //return this.dataStorageService.fetchRecipes();
+      this.store.dispatch(new RecipesActions.FetchRecipes());
+      return this.actions$.pipe(
+        ofType(RecipesActions.FETCH_RECIPES),
+        take(1)
+      )
     } else {
       return recipes;
     }
